2c414846e63c4d1e19ed8078bc45f352e7609f04
Jan De Laet <jan.delaet@dexmach.com>
2024-05-26 18:50:03 +0200

    feat: Initial release

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,19 @@
-FROM mcr.microsoft.com/mcr/hello-world
+FROM python:3.12.3-slim AS build
+
+# test
+WORKDIR /app
+COPY requirements.txt /app/
+ENV PATH="/home/app/.local/bin:${PATH}"
+RUN python -m venv /app/.venv
+ENV PATH="/app/.venv/bin:$PATH"
+RUN pip install --no-cache-dir --upgrade -r requirements.txt
+RUN rm -rf requirements.txt
+
+FROM python:3.12.3-slim
+
+WORKDIR /app
+COPY /app /app/
+COPY --from=build --chown=app:app /app/.venv /app/.venv
+ENV PATH="/app/.venv/bin:$PATH"
+EXPOSE 8000
+CMD ["gunicorn", "--bind", "0.0.0.0:8000", "main:app"]
---
+++ app/main.py
@@ -0,0 +1,43 @@
+import tempfile
+from flask import Flask
+
+app = Flask(__name__)
+
+
+def insecure_file(results):
+    tempfilename = tempfile.mktemp()
+    with open(tempfilename, "wb") as f:
+        f.write(results.encode())
+    print("Results written to", tempfilename.name)
+
+
+def insecure_file2(results):
+    tempfilename2 = tempfile.mktemp()
+    with open(tempfilename2, "wb") as f:
+        f.write(results.encode())
+    print("Results written to", tempfilename2.name)
+
+
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+
+@app.route("/octocat")
+def show_octocat_original():
+    return "<img src='https://octodex.github.com/images/original.png'>"
+
+
+@app.route("/daftpunktocat")
+def show_octocat():
+    return "<img src='https://octodex.github.com/images/daftpunktocat-thomas.gif'>"
+
+
+@app.route("/octocat-hulatocat")
+def show_octocat_hulatocat():
+    return "<img src='https://octodex.github.com/images/hula_loop_octodex03.gif'>"
+
+
+@app.route("/nothing-here")
+def show_image():
+    return "<img src='https://media.giphy.com/media/3o7TKz9bX9v9Kz2wvO/giphy.gif'>"
